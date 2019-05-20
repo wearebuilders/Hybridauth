2ee8f5a690a1612b0dff594773e2deaabb38cc9b
alexbuis <alex@wearebuilders.io>
2019-05-20 21:43:40 +0200

    Added LinkedIN v2 fix.

--- hybridauth/Hybrid/Providers/LinkedIn.php
+++ hybridauth/Hybrid/Providers/LinkedIn.php
@@ -14,7 +14,7 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
     /**
      * {@inheritdoc}
      */
-    public $scope = "r_basicprofile r_emailaddress";
+    public $scope = "r_liteprofile r_emailaddress";
 
     /**
      * {@inheritdoc}
@@ -23,7 +23,7 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
         parent::initialize();
 
         // Provider api end-points.
-        $this->api->api_base_url = "https://api.linkedin.com/v1/";
+        $this->api->api_base_url = "https://api.linkedin.com/v2/";
         $this->api->authorize_url = "https://www.linkedin.com/oauth/v2/authorization";
         $this->api->token_url = "https://www.linkedin.com/oauth/v2/accessToken";
     }
@@ -35,14 +35,22 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
         if (is_array($this->scope)) {
             $this->scope = implode(" ", $this->scope);
         }
-        // redirect the user to the provider authentication url
-        Hybrid_Auth::redirect($this->api->authorizeUrl(array("state" => md5(time()), "scope" => $this->scope)));
+        if (isset($this->scope)) {
+            $extra_params['scope'] = $this->scope;
+        }
+        if (!isset($this->state)) {
+            $this->state = hash("sha256",(uniqid(rand(), TRUE)));
+        }
+        $extra_params['state'] = $this->state;
+        Hybrid_Auth::redirect($this->api->authorizeUrl($extra_params));
     }
 
     /**
      * {@inheritdoc}
      *
-     * @see https://developer.linkedin.com/docs/rest-api
+     * Get user profile fields.
+     *
+     * @see https://docs.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin?context=linkedin/consumer/context
      */
     function getUserProfile() {
         // Refresh tokens if needed.
@@ -50,24 +58,16 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
         $this->refreshToken();
 
         // https://developer.linkedin.com/docs/fields.
-        $fields = isset($this->config["fields"]) ? $this->config["fields"] : [
+        $fields = isset($this->config["fields"]) ? $this->config["fields"] : array(
             "id",
-            "email-address",
-            "first-name",
-            "last-name",
-            "headline",
-            "location",
-            "industry",
-            "picture-url",
-            "public-profile-url",
-        ];
+            "firstName",
+            "lastName",
+            "profilePicture(displayImage~:playableStreams)",
+        );
 
         $this->setHeaders();
-        $response = $this->api->get(
-            "people/~:(" . implode(",", $fields) . ")",
-            array(
-                "format" => "json",
-            )
+        $response = $this->getLinkedinResponse(
+            "me?projection=(" . implode(",", $fields) . ")"
         );
 
         if (!isset($response->id)) {
@@ -77,15 +77,60 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
         $this->user->profile->identifier = isset($response->id) ? $response->id : "";
         $this->user->profile->firstName = isset($response->firstName) ? $response->firstName : "";
         $this->user->profile->lastName = isset($response->lastName) ? $response->lastName : "";
-        $this->user->profile->photoURL = isset($response->pictureUrl) ? $response->pictureUrl : "";
-        $this->user->profile->profileURL = isset($response->publicProfileUrl) ? $response->publicProfileUrl : "";
-        $this->user->profile->email = isset($response->emailAddress) ? $response->emailAddress : "";
-        $this->user->profile->description = isset($response->headline) ? $response->headline : "";
-        $this->user->profile->country = isset($response->location) ? $response->location->name : "";
+        $this->user->profile->email = $this->getUserEmail();
         $this->user->profile->emailVerified = $this->user->profile->email;
+
+        $first_name_lang_code = $response->firstName->preferredLocale->language . '_' . $response->firstName->preferredLocale->country;
+        if (!empty($response->firstName->localized->$first_name_lang_code)) {
+            $this->user->profile->firstName = $response->firstName->localized->$first_name_lang_code;
+        }
+
+        $last_name_lang_code = $response->lastName->preferredLocale->language . '_' . $response->lastName->preferredLocale->country;
+        if (!empty($response->lastName->localized->$last_name_lang_code)) {
+            $this->user->profile->lastName = $response->lastName->localized->$last_name_lang_code;
+        }
+
         $this->user->profile->displayName = trim($this->user->profile->firstName . " " . $this->user->profile->lastName);
 
+        if (!empty($response->profilePicture->{'displayImage~'}->elements)) {
+            $profilePictures = $response->profilePicture->{'displayImage~'}->elements;
+            foreach ($profilePictures as $profilePicture) {
+                if (!empty($profilePicture->data->{'com.linkedin.digitalmedia.mediaartifact.StillImage'}->displaySize->width)) {
+                    // Take last item, that should contain image in highest resolution.
+                    $this->user->profile->photoURL = $profilePicture->identifiers[0]->identifier;
+                }
+            }
+        }
+
         return $this->user->profile;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * Get user email address in separate request.
+     *
+     * @see https://docs.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin?context=linkedin/consumer/context
+     **/
+    function getUserEmail() {
+        // Refresh tokens if needed.
+        $this->setHeaders("token");
+        $this->refreshToken();
+
+        $this->setHeaders();
+
+        // See: https://docs.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin?context=linkedin/consumer/context
+        $response = $this->getLinkedinResponse(
+            'emailAddress?q=members&projection=(elements*(handle~))'
+        );
+
+        if (empty($response->elements[0]->{'handle~'}->emailAddress)) {
+            throw new Exception("User email request failed! {$this->providerId} returned an invalid response: " . Hybrid_Logger::dumpData($response), 6);
+        }
+
+        $email = isset($response->elements[0]->{'handle~'}->emailAddress) ? $response->elements[0]->{'handle~'}->emailAddress : "";
+
+        return $email;
     }
 
     /**
@@ -96,6 +141,7 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
      *   - content: A collection of fields describing the shared content.
      *   - comment: A comment by the member to associated with the share.
      *   - visibility: A collection of visibility information about the share.
+     * @param string $companyId (optional) User company id
      *
      * @return object
      *   An object containing:
@@ -104,7 +150,7 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
      * @throws Exception
      * @see https://developer.linkedin.com/docs/share-on-linkedin
      */
-    function setUserStatus($status) {
+    function setUserStatus($status, $companyId = null) {
         // Refresh tokens if needed.
         $this->setHeaders("token");
         $this->refreshToken();
@@ -116,8 +162,8 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
             }
 
             $this->setHeaders("share");
-            $response = $this->api->post(
-                "people/~/shares?format=json",
+            $url = $companyId ? "companies/{$companyId}/shares?format=json" : "people/~/shares?format=json";
+            $response = $this->api->post($url,
                 array(
                     "body" => $status,
                 )
@@ -166,6 +212,33 @@ class Hybrid_Providers_LinkedIn extends Hybrid_Provider_Model_OAuth2 {
                 );
                 break;
         }
+    }
+
+    /**
+     * Format and sign an oauth for provider api for LinkedIn.
+     *
+     * Don't use the access token for LinkedIn.
+     */
+    protected function getLinkedinResponse($url, $method = "GET", $parameters = array(), $decode_json = true )
+    {
+        if ( strrpos($url, 'http://') !== 0 && strrpos($url, 'https://') !== 0 ) {
+            $url = $this->api->api_base_url . $url;
+        }
+
+        $response = null;
+
+        switch( $method ){
+            case 'GET'  : $response = $this->api->request( $url, $parameters, "GET"  ); break;
+            case 'POST' : $response = $this->api->request( $url, $parameters, "POST" ); break;
+            case 'DELETE' : $response = $this->api->request( $url, $parameters, "DELETE" ); break;
+            case 'PATCH'  : $response = $this->api->request( $url, $parameters, "PATCH" ); break;
+        }
+
+        if( $response && $decode_json ){
+            return $this->api->response = json_decode( $response );
+        }
+
+        return $this->api->response = $response;
     }
 
 }
